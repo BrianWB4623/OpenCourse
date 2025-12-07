e975dc28cbc92913453f3c21c6842cbc46bc96d4
BrianWB4623 <BrianWB4623@icloud.com>
2025-12-06 23:15:15 -0800

    Updated forms.py for LoginForm,RegistrationForm,And AssignmentForm

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,23 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, PasswordField, BooleanField, SubmitField,TextAreaField
+from wtforms.validators import DataRequired,Optional
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Sign in')
+
+class RegistrationForm(FlaskForm):
+    username=StringField('Username',validators=[DataRequired()])
+    password=PasswordField('Password',validators=[DataRequired()])
+    role=StringField('Role',validators=[Optional()])
+    submit=SubmitField('Register User')
+class AssignmentForm(FlaskForm):
+    name=StringField('Assignment Name',validators=[DataRequired()])
+    description=TextAreaField('Assignment Description',validators=[Optional()])
+    submit=SubmitField('Publish Assignment')
+
+
+
+
